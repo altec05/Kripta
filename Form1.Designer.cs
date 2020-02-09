8e5090b232c368a8e692502c9214baffef71da60
altec05 <domashenko213@gmail.com>
2020-02-09 19:00:48 +0700

    Шифр Цезаря
    -Добавлен "шифр Цезаря", как пробная работа.
    -Изменён внешний вид формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,72 +35,137 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(466, 86);
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(337, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Действие";
+            this.button1.Text = "Шифровать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.Size = new System.Drawing.Size(122, 21);
             this.label1.TabIndex = 1;
             this.label1.Text = "Введите текст";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(632, 45);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.label2.Location = new System.Drawing.Point(671, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.Size = new System.Drawing.Size(178, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "Обработанный текст";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 88);
+            this.textBox1.Location = new System.Drawing.Point(4, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(325, 378);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(236, 87);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Шифр Цезаря",
+            "Шифр 2"});
+            this.comboBox1.Location = new System.Drawing.Point(337, 48);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(227, 27);
             this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(245, 45);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.label3.Location = new System.Drawing.Point(378, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.Size = new System.Drawing.Size(140, 21);
             this.label3.TabIndex = 5;
             this.label3.Text = "Выберите шифр";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(635, 88);
+            this.textBox2.Location = new System.Drawing.Point(572, 47);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(376, 378);
             this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(452, 88);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Дешифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(337, 225);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(227, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Visible = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(345, 200);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Укажите шаг шифрования";
+            this.label4.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(953, 429);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -108,7 +173,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Обработка текста";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -124,6 +194,9 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,112 @@ namespace Ciphers
         public Form1()
         {
             InitializeComponent();
+
+            //comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        static string al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789 ,.!?-+_=№#'$%^:*()";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Шифр Цезаря":
+                    textBox2.Text = Orientation(textBox1.Text); //Введённое в tb1 попадёт потом в tb2
+                    break;
+                case "Шифр 2":
+                    textBox2.Text = "ВЕДЁТСЯ РАЗРАБОТКА ШИФРОВКИ ДЛЯ ЭТОГО ШИФРА!";
+                    break;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Шифр Цезаря":
+                    textBox2.Text = Orientation2(textBox1.Text); //Введённое в tb1 попадёт потом в tb2
+                    break;
+                case "Шифр 2":
+                    textBox2.Text = "ВЕДЁТСЯ РАЗРАБОТКА ДЕШИФРОВКИ ДЛЯ ЭТОГО ШИФРА!";
+                    break;
+            }
+            
+        }
+
+        public string Orientation(string inp) //ori - для обозачения боока в котором происходит шифрование
+        {
+            int step = 0;
+            StringBuilder code = new StringBuilder();
+            string s = textBox1.Text; // s - связана с вводимым текстом
+            string sd = textBox3.Text; //sd-количество шагов шифра (ключ шифрования)
+            if(textBox3.Text == "")
+            {
+                MessageBox.Show("Укажите требуемый шаг!");
+            }
+            else
+            {
+                step = Convert.ToInt32(sd);
+                for (int i = 0; i < s.Length; i++)
+                    for (int j = 0; j < al.Length; j++)
+                        if (s[i] == al[j]) code.Append(al[(j + step) % al.Length]);
+            }
+            return code.ToString();
+        }
+
+        public string Orientation2(string inp) //ori - для обозачения боока в котором происходит шифрование
+        {
+            int step = 0;
+            StringBuilder code = new StringBuilder();
+            string s = textBox1.Text; // s - связана с вводимым текстом
+            string sd = textBox3.Text; //sd-количество шагов шифра (ключ шифрования)
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Укажите требуемый шаг!");
+            }
+            else
+            {
+                step = Convert.ToInt32(sd);
+                for (int i = 0; i < s.Length; i++)
+                    for (int j = 0; j < al.Length; j++)
+                        if (s[i] == al[j]) code.Append(al[(j - step + al.Length) % al.Length]);
+            }
+            return code.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedState = comboBox1.SelectedItem.ToString();
+            if(selectedState != "Шифр Цезаря")
+            {
+                label4.Visible = false;
+                textBox3.Visible = false;
+            }
+            else
+            {
+                label4.Visible = true;
+                textBox3.Visible = true;
+            }
+            //MessageBox.Show(selectedState);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
